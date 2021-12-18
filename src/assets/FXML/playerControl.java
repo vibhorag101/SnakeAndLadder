c8cb186cfae6bcf26fb3ffa8c2e04e77e9906c47
pritishposwal <pritish20321@iiitd.ac.in>
2021-12-18 11:05:09 +0530

    snake function
    making snake movement functions

--- src/assets/FXML/playerControl.java
+++ src/assets/FXML/playerControl.java
@@ -40,6 +40,105 @@ public class playerControl {
         pathTransition.setCycleCount(1);
         pathTransition.play();
     }
+<<<<<<< Updated upstream
+=======
+    public int getLadderDest(int Start){
+        return switch (Start) {
+            case 4 -> 25;
+            case 8 -> 31;
+            case 28 -> 46;
+            case 32 -> 48;
+            case 42 -> 80;
+            case 52 -> 68;
+            case 58 -> 77;
+            case 69 -> 93;
+            case 84 -> 98;
+            default -> 0;
+        };
+    }
+    public int getSnakeDest(int Start){
+        return switch (Start) {
+            case 11 -> 9;
+            case 36 -> 14;
+            case 43 -> 22;
+            case 56 -> 18;
+            case 76 -> 55;
+            case 81 -> 69;
+            case 90 -> 50;
+            case 94 -> 53;
+            case 97 -> 64;
+            case 99 -> 78;
+            default -> 0;
+        };
+    }
+    public Path getLadderPath(int intitialPos ,int ladderStart){
+        Path path = new Path();
+        ObservableList<PathElement> elementsPath = gb.getMovingPath(intitialPos, ladderStart).getElements();
+        ObservableList<PathElement> elementsPathLadder = gb.pathMaker(ladderStart, getLadderDest(ladderStart)).getElements();
+        elementsPath.addAll(elementsPathLadder);
+
+        for (PathElement element : elementsPath) {
+            path.getElements().add(element);
+        }
+        return path;
+    }
+    public void playAnimationOnLadder(Circle p, int pPos, int pDest) {
+        PathTransition pathTransition = new PathTransition();
+        Path path = new Path();
+        if (pDest== 4){
+            path = getLadderPath(pPos, pDest);
+        }
+        else if (pDest== 8){
+            path = getLadderPath(pPos, pDest);
+        }
+        else if (pDest== 28){
+            path = getLadderPath(pPos, pDest);
+        }
+        else if (pDest== 32){
+            path = getLadderPath(pPos, pDest);
+        }
+        else if (pDest== 42){
+            path = getLadderPath(pPos, pDest);
+        }
+        else if (pDest== 52){
+            path = getLadderPath(pPos, pDest);
+        }
+        else if (pDest== 58){
+            path = getLadderPath(pPos, pDest);
+        }
+        else if (pDest== 69){
+            path = getLadderPath(pPos, pDest);
+        }
+        else if (pDest== 84){
+            path = getLadderPath(pPos, pDest);
+        }
+        pathTransition.setPath(path);
+        pathTransition.setAutoReverse(false);
+        pathTransition.setNode(p);
+        pathTransition.setDuration(Duration.seconds(3));
+        pathTransition.setCycleCount(1);
+        pathTransition.play();
+    }
+    public boolean checkOnLadder(int Destination){
+        if (Destination==4 || Destination == 8 || Destination == 28 || Destination== 32
+        || Destination == 42 || Destination ==58 || Destination == 52 || Destination == 69 ||
+                Destination == 84) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean checkOnSnake(int Destination){
+        if (Destination==11 || Destination == 36 || Destination == 43 || Destination == 56 ||
+                Destination == 76 || Destination == 81 || Destination == 90 || Destination == 94 || Destination == 97 || Destination == 99) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+>>>>>>> Stashed changes
 
     public void movePlayer(int diceNum){
         if (p1Turn) {
@@ -49,10 +148,33 @@ public class playerControl {
             p1Turn = false;
             p2Turn = true;
             if (p1Unlocked) {
+<<<<<<< Updated upstream
                 int p1Dest = p1Pos + diceNum;
                 if (p1Dest <= 100) {
                     playAnimation(p1, p1Pos, p1Dest);
                     p1Pos = p1Dest;
+=======
+                boolean onLadder;
+                boolean onSnake;
+                int p1Dest = p1Pos + diceNum;
+                if (p1Dest <= 100) {
+                    onLadder = checkOnLadder(p1Dest);
+                    onSnake = checkOnSnake(p1Dest);
+                    System.out.println("Ladder "+onLadder);
+                    System.out.println("Snake "+onSnake);
+                    if(onLadder){
+                        playAnimationOnLadder(p1, p1Pos, p1Dest);
+                        p1Pos = getLadderDest(p1Dest);
+                    }
+                    else if(onSnake){
+                        p1Pos = getSnakeDest(p1Dest);
+                    }
+                    else{
+                        playAnimation(p1, p1Pos, p1Dest);
+                        p1Pos = p1Dest;
+                    }
+
+>>>>>>> Stashed changes
                 }
                 if (p1Dest==100){
                     p1Won = true;
